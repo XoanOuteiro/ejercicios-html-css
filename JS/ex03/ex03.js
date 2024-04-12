d2c89402c10ddebdf9b9b3b1d0e9f540cc55ccea
XoanOuteiro <xoanouteirojorge@gmail.com>
2024-04-12 11:15:08 +0200

    solucion ex03 JS

---
+++ JS/ex03/ex03.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + "/"
+                    + (currentdate.getMonth()+1)  + "/" 
+                    + currentdate.getFullYear() + " @ "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+    document.getElementById("contenido").innerText = datetime;
+}, 1000); 
